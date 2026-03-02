b8247e6e8ccc13ac496444a7f04cad7de32d3171
0602d <0602dag@gmail.com>
2026-03-02 10:27:26 +0100

    leganes terminado

--- src/proyectoLiga/principal/Main.java
+++ src/proyectoLiga/principal/Main.java
@@ -163,9 +163,11 @@ public class Main {
         madrid.getJugadores().add(new Jugador("Antonio","Rüdiger","Alemania",3,LocalDate.of(1993,3,3),22,"28001",null,1,Posicion.DEFENSA));
         madrid.getJugadores().add(new Jugador("David","Alaba","Australia",4,LocalDate.of(1992,6,24),4,"28001",null,1,Posicion.DEFENSA));
         madrid.getJugadores().add(new Jugador("Ferland","Mendy","Francia",5,LocalDate.of(1995,6,8),23,"28001",null,1,Posicion.DEFENSA));
+
         madrid.getJugadores().add(new Jugador("Jude","Bellingham","Inglaterra",6,LocalDate.of(2003,6,29),5,"28001",null,1,Posicion.CENTROCAMPISTA));
         madrid.getJugadores().add(new Jugador("Federico","Valverde","Uruguay",7,LocalDate.of(1998,7,22),15,"28001",null,1,Posicion.CENTROCAMPISTA));
         madrid.getJugadores().add(new Jugador("Eduardo","Camavinga","Francia",8,LocalDate.of(2002,11,10),12,"28001",null,1,Posicion.CENTROCAMPISTA));
+
         madrid.getJugadores().add(new Jugador("Vinicius","Junior","Brasil",9,LocalDate.of(2000,7,12),7,"28001",null,1,Posicion.DELANTERO));
         madrid.getJugadores().add(new Jugador("Rodrygo","Goes","Brasil",10,LocalDate.of(2001,1,9),11,"28001",null,1,Posicion.DELANTERO));
         madrid.getJugadores().add(new Jugador("Kylian","Mbappé","Francia",11,LocalDate.of(1998,12,20),9,"28001",null,1,Posicion.DELANTERO));
@@ -181,14 +183,38 @@ public class Main {
         barca.getJugadores().add(new Jugador("Ronald","Araújo","Uruguay",14,LocalDate.of(1999,3,7),4,"08001",null,2,Posicion.DEFENSA));
         barca.getJugadores().add(new Jugador("Pau","Cubarsí","España",15,LocalDate.of(2007,1,22),2,"08001",null,2,Posicion.DEFENSA));
         barca.getJugadores().add(new Jugador("Alejandro","Balde","España",16,LocalDate.of(2003,10,18),3,"08001",null,2,Posicion.DEFENSA));
+
         barca.getJugadores().add(new Jugador("Frenkie","de Jong","Países Bajos",17,LocalDate.of(1997,5,12),21,"08001",null,2,Posicion.CENTROCAMPISTA));
         barca.getJugadores().add(new Jugador("Pedri","González","España",18,LocalDate.of(2002,11,25),8,"08001",null,2,Posicion.CENTROCAMPISTA));
         barca.getJugadores().add(new Jugador("Gavi","Páez","España",19,LocalDate.of(2004,8,5),6,"08001",null,2,Posicion.CENTROCAMPISTA));
+
         barca.getJugadores().add(new Jugador("Lamine","Yamal","España",20,LocalDate.of(2007,7,13),27,"08001",null,2,Posicion.DELANTERO));
         barca.getJugadores().add(new Jugador("Robert","Lewandowski","Polonia",21,LocalDate.of(1988,8,21),9,"08001",null,2,Posicion.DELANTERO));
         barca.getJugadores().add(new Jugador("Raphinha","Dias","Brasil",22,LocalDate.of(1996,12,14),11,"08001",null,2,Posicion.DELANTERO));
 
         equipos.add(barca);
+
+
+        Equipo leganes = new Equipo(3, "Cd Leganes", "Leganes", "Ontime Butarque", 3);
+        Entrenador oca = new Entrenador("Igor", "Oca", "España", 3);
+        leganes.setEntrenador(oca);
+
+        leganes.getJugadores().add(new Jugador("Juan","Soriano","España",23,LocalDate.of(1997,8,23),1,"28918",null,3,Posicion.PORTERO));
+        leganes.getJugadores().add(new Jugador("Enric","Franquesa","España",24,LocalDate.of(1997,2,26),15,"28918",null,3,Posicion.DEFENSA));
+        leganes.getJugadores().add(new Jugador("Ignasi","Miquel","España",25,LocalDate.of(1992,9,28),5,"28918",null,3,Posicion.DEFENSA));
+        leganes.getJugadores().add(new Jugador("Gonzalo","Aguilar","España",26,LocalDate.of(2002,6,11),6,"28918",null,3,Posicion.DEFENSA));
+        leganes.getJugadores().add(new Jugador("Sebastián","Figueredo","Uruguay",27,LocalDate.of(2001,8,25),22,"28918",null,3,Posicion.DEFENSA));
+
+        leganes.getJugadores().add(new Jugador("Óscar","Plano","España",28,LocalDate.of(1991,2,11),20,"28918",null,3,Posicion.CENTROCAMPISTA));
+        leganes.getJugadores().add(new Jugador("Dani","Rodríguez","España",29,LocalDate.of(1988,6,6),9,"28918",null,3,Posicion.CENTROCAMPISTA));
+        leganes.getJugadores().add(new Jugador("Amadou","Diawara","Guinea Ecuatorial",30,LocalDate.of(1997,7,17),24,"28918",null,3,Posicion.CENTROCAMPISTA));
+
+        leganes.getJugadores().add(new Jugador("Naím","García","España",31,LocalDate.of(2002,6,11),17,"28918",null,3,Posicion.DELANTERO));
+        leganes.getJugadores().add(new Jugador("Luis","Asué","Guinea Ecuatorial",32,LocalDate.of(2001,7,9),12,"28918",null,3,Posicion.DELANTERO));
+        leganes.getJugadores().add(new Jugador("Juan","Cruz","España",33,LocalDate.of(2000,4,25),10,"28918",null,3,Posicion.DELANTERO));
+
+        equipos.add(leganes);
+
         return equipos;
     }
 
